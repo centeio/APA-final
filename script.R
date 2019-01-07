97f51e5a527b5d810ca3aa515952f5d46ec33c18
centeio <c.centeiojorge@outlook.pt>
2019-01-07 17:17:27 +0100

    data preprocessing and starting methods

--- script.R
+++ script.R
@@ -48,15 +48,67 @@ colnames(Adult.factors) <- colnames(Adult.complete)
 
 Adult.numeric <- data.frame(Adult.complete$age, Adult.complete$education,
                        Adult.complete$educationnum, Adult.complete$maritalstatus, Adult.complete$relationship, 
-                       Adult.complete$race, Adult.complete$sex, Adult.complete$capitalgain, Adult.complete$capitalloss, 
+                       Adult.complete$race, Adult.complete$capitalgain, Adult.complete$capitalloss, 
                        Adult.complete$hoursweek, Adult.complete$class)
 
 Adult.factors <- data.frame(Adult.factors$age, Adult.factors$education,
                             Adult.factors$educationnum, Adult.factors$maritalstatus, Adult.factors$relationship, 
-                            Adult.factors$race, Adult.factors$sex, Adult.factors$capitalgain, Adult.factors$capitalloss, 
+                            Adult.factors$race, Adult.factors$capitalgain, Adult.factors$capitalloss, 
                             Adult.factors$hoursweek, Adult.factors$class)
 
-summary(Adult.factors)
+#take out the feature "sex" because it has a strong correlation with the feature "relationship" and has a lower correlation with "class" than the latter
 
 
+##############################
+#### STATISTICAL ANALYSIS ####
+##############################
 
+library('psych')
+
+psych::describe(Adult.numeric)
+
+
+#summary
+summary(Adult.factors) 
+
+
+##############################
+####### Training / Test ######
+##############################
+
+#TODO: check differences without oversampling the test sample
+
+t <-sample(1:nrow(Adult.numeric),round(0.02*nrow(Adult.numeric),digits=0))
+t2 <-sample(1:nrow(Adult.numeric),round(0.002*nrow(Adult.numeric),digits=0))
+traindata.numeric <-Adult.numeric[t, ]
+testdata.numeric <-Adult.numeric[t2, ]
+
+traindata <-Adult.factors[t, ] #factor
+testdata <-Adult.factors[t2, ]
+
+
+#### Algorithms #####
+
+###### Linear #######
+
+######## SVM ########
+
+library('e1071')
+
+model.train <- as.matrix(traindata.numeric)
+
+for (C in 10^seq(-2,3))
+{
+  modelmenor35linear <- svm(model.train[,-10], model.train[,10], 
+                            type="C-classification", 
+                            cost=0.1, kernel="linear", 
+                            scale = FALSE)
+  pred <- predict(modelmenor35linear,model.train[,-10])
+  t_true <- model.train[,10]
+  table(pred,t_true)
+  print(paste("C:",C,"  error:",(sum(pred != t_true)/length(t_true))*100,"%"))
+}
+
+######## MLP
+
+###### Non-linear ######
